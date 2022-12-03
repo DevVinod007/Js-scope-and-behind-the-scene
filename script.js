08b689cd73c78e7bd1dfdfd6f05eab817306a946
Vinod <vinod8797@gmail.com>
2022-12-03 22:18:35 +0530

    added hoisting changes

--- script.js
+++ script.js
@@ -36,3 +36,53 @@ const firstName = "vinod";
 console.log(calAge(1994));
 // console.log(age);
 // printAge()
+
+
+
+
+
+//Hoisting
+
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+// var x = 1;
+// // const y = 1;
+// let z = 1;
+
+
+// console.log(decF(2,5));
+// console.log(addExp(2,5));
+// console.log(addArrow(2,5));
+
+// function decF(a,b){
+//     return a+b;
+// }
+
+// const addExp = function(d,c){
+//     return d+c;
+// }
+
+// var addArrow = (a,b) =>{
+//     a+b
+// } 
+
+// if(!count) DeleteallData()
+// console.log(count);
+// var count = 10;
+// function DeleteallData(){
+//     console.log('Delete all data in data base ')
+// }
+
+
+//Window usage 
+
+var x = 1;
+const y = 1;
+let z = 1;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.y);
+
